377662065fd507b0ba8bbe1d3c254cc278a62594
LEGRAND Matthieu <legrand.ma@chu-toulouse.fr>
2023-05-12 10:40:53 +0200

    improving the run speed for the glcm on cpu and gpu

--- src/glcm.rs
+++ src/glcm.rs
@@ -1,5 +1,7 @@
 use tch::{Kind, Tensor, index::*};
 
+use crate::tensor_ext::TensorExt;
+
 /**
 Computes the gray-level co-occurrence matrix (GLCM) of an image.
 
@@ -23,14 +25,19 @@ The GLCM is a 2D histogram of the co-occurrence of pixel values at a given offse
     The values are in the range [0, 1] and represent the normalized GLCM.
     dim 1 is the reference shade and dim 2 is the neighbor shade.
 
-# Big O
-- Time: O(num_shades²) if we have N * W * H threads.
-
  */
 pub fn glcm(image: &Tensor, offset: (i64, i64), num_shades: u8, mask: Option<&Tensor>, symmetric: bool) -> Tensor {
+    if image.device().is_cuda(){
+        glcm_gpu(image, offset, num_shades, mask, symmetric)
+    } else {
+        glcm_cpu(image, offset, num_shades, mask, symmetric)
+    }
+}
+
+pub fn glcm_gpu(image: &Tensor, offset: (i64, i64), num_shades: u8, mask: Option<&Tensor>, symmetric: bool) -> Tensor {
     let (offset_y, offset_x) = offset;
     let (offset_y, offset_x) = (offset_y as i64, offset_x as i64);
-    let (batch_size, _, height, width) = image.size4().unwrap();
+    let (_, _, height, width) = image.size4().unwrap();
 
     let mut image = image * num_shades as f64;
     // If we use a mask we add 255 to the masked pixels so that they are not counted in the GLCM.
@@ -54,18 +61,12 @@ pub fn glcm(image: &Tensor, offset: (i64, i64), num_shades: u8, mask: Option<&Te
         offset_x.max(0)..(width+offset_x).min(width),
     );    
     
-    let mut glcm = Tensor::zeros(
-        &[batch_size, num_shades as i64, num_shades as i64],
-        (Kind::Float, image.device()),
-    );
-    for reference_shade in 0..num_shades {
-        let reference_mask = image.eq(reference_shade as i64);
-        for neighbor_shade in 0..num_shades {
-            let neighbor_mask = image.eq(neighbor_shade as i64); 
-            let mut slice = glcm.i((.., reference_shade as i64, neighbor_shade as i64));
-            slice += (reference_mask.i(rslice.clone()) * neighbor_mask.i(nslice.clone())).sum_dim_intlist(Some(&[1, 2, 3][..]), false, Kind::Float);
-        }
-    }
+    let ref_img = image.i(rslice.clone());
+    let neigh_img = image.i(nslice.clone());
+    let shades = Tensor::arange(num_shades as i64, (Kind::Float, image.device())).view([1, num_shades as i64, 1, 1]);
+    let ref_img = ref_img.eq_tensor(&shades).unsqueeze(2); // [N, S, 1, H, W]
+    let neigh_img = neigh_img.eq_tensor(&shades).unsqueeze(1); // [N, 1, S, H, W]
+    let mut glcm = (ref_img * neigh_img).sum_dims([-1, -2]); // [N, S, S]
 
     if symmetric {
         glcm+=glcm.copy().transpose(1, 2);
@@ -76,9 +77,54 @@ pub fn glcm(image: &Tensor, offset: (i64, i64), num_shades: u8, mask: Option<&Te
     &glcm / len
 }
 
+pub fn glcm_cpu(image: &Tensor, offset: (i64, i64), num_shades: u8, mask: Option<&Tensor>, symmetric: bool) -> Tensor{
+    let (offset_y, offset_x) = offset;
+
+    let (batch_size, _, height, width) = image.size4().unwrap();
+    let (batch_size, height, width) = (batch_size as usize, height as usize, width as usize);
+
+    let mut image = image * num_shades as f64;
+    // If we use a mask we add 255 to the masked pixels so that they are not counted in the GLCM.
+    if let Some(mask) = mask {
+        let mask = (mask - 1.0) * -255.0;
+        image += mask;
+    }
+    let image = image.clamp(0.0, 255.0).to_kind(Kind::Uint8);
+
+    let batch_span = height * width;
+
+    // Moving from Tensor to Vec<u8>
+    let image = Vec::<u8>::from(&image);
+    let it =((-offset_y).max(0)..(height as i64-offset_y).min(height as i64))
+        .flat_map(|y| ((-offset_x).max(0)..(width as i64-offset_x).min(width as i64))
+            .map(move|x| (y as usize, x as usize))
+        );
+
+    let glcm = (0..batch_size)
+        .map(|batch|{
+            let mut glcm = vec![0; num_shades as usize * num_shades as usize];
+            for (y, x) in it.clone(){
+                let reference_shade = image[batch * batch_span + y * width + x] as usize;
+                let neighbor_shade = image[batch * batch_span + (y + offset_y as usize) * width + (x + offset_x as usize)] as usize;
+                if reference_shade >= num_shades as usize || neighbor_shade >= num_shades as usize{
+                    continue;
+                }
+                glcm[reference_shade * num_shades as usize + neighbor_shade] += 1;
+            }
+            Tensor::of_slice(&glcm).view([num_shades as i64, num_shades as i64])
+        });
+    let mut glcm = Tensor::stack(&glcm.collect::<Vec<_>>()[..], 0);
+    if symmetric {
+        glcm+=glcm.copy().transpose(0, 1);
+    }
+    let len = glcm.sum_dim_intlist(Some(&[1, 2][..]), false, Kind::Float);
+    let len = len.view([-1, 1, 1]);
+    glcm / len
+}
+
 #[cfg(test)]
 mod test {
-    use tch::Tensor;
+    use tch::{Tensor};
 
     use crate::utils::assert_eq_tensor;
 
@@ -99,8 +145,12 @@ mod test {
         
         let input = (input.view((1, 1, 4, 4))-1.0) / 3.0;
         let glcm = super::glcm(&input, (1, 0), 3, None, false);
+        let glcm_gpu = super::glcm_gpu(&input, (1, 0), 3, None, false);
+        let glcm_cpu = super::glcm_cpu(&input, (1, 0), 3, None, false);
 
         assert_eq_tensor(&glcm, &expected);
+        assert_eq_tensor(&glcm_gpu, &expected);
+        assert_eq_tensor(&glcm_cpu, &expected);
     }
 
     #[test]
