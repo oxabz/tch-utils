af9ba48db3ce62e33949d91d06238c49cbc0ca7d
LEGRAND Matthieu <legrand.ma@chu-toulouse.fr>
2023-05-12 16:20:09 +0200

    updating to a better glcm function for the gpu

--- src/glcm.rs
+++ src/glcm.rs
@@ -1,6 +1,6 @@
 use tch::{Kind, Tensor, index::*};
 
-use crate::tensor_ext::TensorExt;
+const GLCM_BINCOUNT_SIZE: i64 = 0x00FF_FFFF;
 
 /**
 Computes the gray-level co-occurrence matrix (GLCM) of an image.
@@ -12,7 +12,7 @@ The GLCM is a 2D histogram of the co-occurrence of pixel values at a given offse
     The values are expected to be in the range [0, 1].
 - offset: The offset of the co-occurrence. The first element is the vertical offset and the second element is the horizontal offset.
 - num_shades: The number of shades of gray to use for the GLCM.
-    The values are expected to be in the range [2, 254], if a mask is used and [2, 255] if not.
+    The values are expected to be in the range [2, 254]
     Internally the image will be cast from float to u8 and the value 255 is used to represent masked pixels.
 - mask: A mask of shape [N, 1, H, W] where N is the batch size, H and W are the height and width of the image.
     The values are expected to be in the range [0, 1]. If provided, the GLCM will only be computed for the pixels where the mask is 1.
@@ -34,18 +34,17 @@ pub fn glcm(image: &Tensor, offset: (i64, i64), num_shades: u8, mask: Option<&Te
     }
 }
 
-pub fn glcm_gpu(image: &Tensor, offset: (i64, i64), num_shades: u8, mask: Option<&Tensor>, symmetric: bool) -> Tensor {
+pub fn glcm_gpu(image: &Tensor, offset: (i64, i64), num_shades: u8, mask: Option<&Tensor>, symmetric: bool) -> Tensor{
     let (offset_y, offset_x) = offset;
     let (offset_y, offset_x) = (offset_y as i64, offset_x as i64);
-    let (_, _, height, width) = image.size4().unwrap();
+    let (batch_size, _, height, width) = image.size4().unwrap();
 
-    let mut image = image * num_shades as f64;
-    // If we use a mask we add 255 to the masked pixels so that they are not counted in the GLCM.
+    let mut image = image * (num_shades as f64 - 1e-6);
     if let Some(mask) = mask {
-        let mask = (mask - 1.0) * -255.0;
+        let mask = (mask - 1.0) * -(num_shades as f64);
         image += mask;
     }
-    let image = image.clamp(0.0, 255.0).to_kind(Kind::Uint8);
+    let image = image.floor().clamp(0.0, 255.0).to_kind(Kind::Uint8);
 
     // preping the slices for the image
     let rslice = (
@@ -61,15 +60,37 @@ pub fn glcm_gpu(image: &Tensor, offset: (i64, i64), num_shades: u8, mask: Option
         offset_x.max(0)..(width+offset_x).min(width),
     );    
     
-    let ref_img = image.i(rslice.clone());
-    let neigh_img = image.i(nslice.clone());
-    let shades = Tensor::arange(num_shades as i64, (Kind::Float, image.device())).view([1, num_shades as i64, 1, 1]);
-    let ref_img = ref_img.eq_tensor(&shades).unsqueeze(2); // [N, S, 1, H, W]
-    let neigh_img = neigh_img.eq_tensor(&shades).unsqueeze(1); // [N, 1, S, H, W]
-    let mut glcm = (ref_img * neigh_img).sum_dims([-1, -2]); // [N, S, S]
+    // We take a shifted version of the image and compute the GLCM for each pixel.
+    let ref_img = image.i(rslice.clone()).to_kind(Kind::Int64);
+    let neigh_img = image.i(nslice.clone()).to_kind(Kind::Int64);
+
+    // We then encode the values into a single number.
+    // Saddly libtorch doesn't support uint32 tensors so we have to use int32.
+    // We map the neighbor's value to the first 8 bits and the reference's value to the next 8 bits.
+    // We then use an other 8 bits to encode the batch index we coulduse 15 bit for the batch but it would result in a gigabyte sized tensor.
+    let num_shades = (num_shades + 1) as i64; // Number of shades + 1 for the masked pixels
+    let group_size = ((GLCM_BINCOUNT_SIZE-1) / num_shades.pow(2)).min(batch_size);
+    println!("group_size: {}", group_size);
+    let group_count = batch_size / group_size + (batch_size % group_size != 0) as i64;
+    println!("group_count: {}", group_count);
+    let batch_idx = Tensor::arange(batch_size as i64, (Kind::Int64, image.device())).remainder(group_size);
+    let batch_idx = batch_idx.view([-1, 1, 1, 1]);
+    let pairs = batch_idx * num_shades.pow(2) + ref_img * num_shades as i64 + neigh_img;
+
+    let glcms = {
+        // We split the tensor into groups of size group_size.
+        let pairs = pairs.tensor_split(group_count, 0);
+        let bincount_size = num_shades.pow(2) * group_size;
+        pairs.iter()
+            .map(|t| t.view([-1]))
+            .map(|t| t.bincount::<&Tensor>(None, bincount_size))
+            .map(|t| t.view([-1, num_shades as i64, num_shades as i64]))
+            .collect::<Vec<_>>()
+    };
+    let mut glcm = Tensor::cat(&glcms[..], 0).i((.., ..num_shades-1, ..num_shades-1));
 
     if symmetric {
-        glcm+=glcm.copy().transpose(1, 2);
+        glcm+=glcm.copy().transpose(-1, -2);
     }
 
     let len = glcm.sum_dim_intlist(Some(&[1, 2][..]), false, Kind::Float);
@@ -125,13 +146,23 @@ pub fn glcm_cpu(image: &Tensor, offset: (i64, i64), num_shades: u8, mask: Option
 
 #[cfg(test)]
 mod test {
-    use tch::{Tensor};
+    use tch::{Tensor, Kind, index::*};
 
     use crate::utils::assert_eq_tensor;
 
     #[test]
+    fn bidouillage(){
+        
+    }
+
+    #[test]
     fn test_glcm_no_mask(){
         let input = Tensor::of_slice(&[
+            1.0, 1.0, 2.0, 1.0,
+            2.0, 1.0, 1.0, 1.0,
+            3.0, 2.0, 3.0, 1.0,
+            3.0, 2.0, 1.0, 2.0,             
+
             1.0, 1.0, 2.0, 1.0,
             2.0, 1.0, 1.0, 1.0,
             3.0, 2.0, 3.0, 1.0,
@@ -142,12 +173,23 @@ mod test {
             0.25, 0.17, 0.0,
             0.25, 0.0,  0.08,
             0.08, 0.17, 0.0,
-        ]).view((1, 3, 3));
+
+            0.25, 0.17, 0.0,
+            0.25, 0.0,  0.08,
+            0.08, 0.17, 0.0,
+        ]).view((2, 3, 3));
         
-        let input = (input.view((1, 1, 4, 4))-1.0) / 3.0;
+        let input = (input.view((2, 1, 4, 4))-1.0) / 3.0;
+        let rand = Tensor::rand(&[2, 1, 4, 4], (Kind::Float, tch::Device::Cpu));
+        let input = Tensor::cat(&[input, rand], 0);
         let glcm = super::glcm(&input, (1, 0), 3, None, false);
         let glcm_gpu = super::glcm_gpu(&input, (1, 0), 3, None, false);
         let glcm_cpu = super::glcm_cpu(&input, (1, 0), 3, None, false);
+
+        let glcm = glcm.i(..2);
+        let glcm_gpu = glcm_gpu.i(..2);
+        let glcm_cpu = glcm_cpu.i(..2);
+
 
         assert_eq_tensor(&glcm, &expected);
         assert_eq_tensor(&glcm_gpu, &expected);
